31164edcbd78bed20544c341305dd808aeaaf458
Johnnybar <johnnybareket@gmail.com>
2022-02-24 22:02:34 +0000

    develop - adds header, footer, styling to components

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Footer.scss";
+
+function Footer() {
+  return (
+    <footer className="opinary-footer">
+      <div className="opinary-footer__text text-center">
+        Opinary - We help people make up their minds
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import logo from "../../assets/opinary-logo.svg";
+import "./Header.scss";
+
+function Header() {
+  return (
+    <header className="opinary-header w-100">
+      <div className="opinary-header__content-wrapper">
+        <img src={logo} alt="opinary logo" className="opinary-header__logo" />
+        <h3 className="opinary-header__heading  text-center ">Opinary Poll</h3>
+
+        <div></div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
