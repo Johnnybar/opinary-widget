d352fc9fefa8400f71e1940cbc83454cf3075d24
Johnnybar <johnnybareket@gmail.com>
2022-02-24 22:03:41 +0000

    develop - adds animation and button styling to Answer

--- src/components/Answer/Answer.tsx
+++ src/components/Answer/Answer.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import "./Answer.scss";
 import { Card, Button } from "react-bootstrap";
+import { motion } from "framer-motion";
 
 function Answer({ answer, vote, id, incrementVoteCount }: AnswerProps) {
   return (
     <Card>
       <Card.Body>
         <Card.Title>{answer}</Card.Title>
-        <Button variant="success" onClick={() => incrementVoteCount(id)}>
+        <motion.button
+          className="opinary-answer__button text-center"
+          whileHover={{ scale: 1.2 }}
+          whileTap={{ rotate: "-40deg" }}
+          onClick={() => incrementVoteCount(id)}
+        >
           Vote
-        </Button>
+        </motion.button>
       </Card.Body>
       <Card.Footer>Vote count: {vote}</Card.Footer>
     </Card>
