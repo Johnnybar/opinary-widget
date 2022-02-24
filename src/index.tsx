cd5d65f04daf3ee95174203e1d1af8bada7e2ba3
Johnnybar <johnnybareket@gmail.com>
2022-02-24 20:08:15 +0000

    develop - adds remove identical polls functionality

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,29 @@ import "./index.scss";
 import App from "./components/App/App";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const WidgetDivs = document.querySelectorAll("#opinary-widget");
+const widgetDivs = document.querySelectorAll("#opinary-widget");
+
+//Check if polls are identical, remove the identical ones
+const removeIdenticalNodes = (widgetNodeList: NodeListOf<Element>) => {
+  let widgetArr = Array.from(widgetNodeList);
+  for (let i = 0; i < widgetArr.length; i++) {
+    for (let j = 1; j < widgetArr.length; j++) {
+      if (widgetArr[i].isEqualNode(widgetArr[j])) {
+        widgetArr.splice(widgetArr.indexOf(widgetArr[i]), 1);
+      }
+    }
+  }
+  return widgetArr;
+};
+
+const uniqueWidgetDivs = removeIdenticalNodes(widgetDivs);
 
 // Inject our React App into each
-WidgetDivs.forEach((Div) => {
+uniqueWidgetDivs.forEach((div) => {
   ReactDOM.render(
     <React.StrictMode>
-      <App domElement={Div} />
+      <App domElement={div} />
     </React.StrictMode>,
-    Div
+    div
   );
 });
