1f29bba5488a82a7f35309025ab14df73554447c
Johnnybar <johnnybareket@gmail.com>
2022-02-24 20:10:25 +0000

    develop - completes localstorage functionality with exactId, adds error handling, moves interfaces out

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,29 +1,19 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import logo from "./logo.svg";
 import Answer from "../Answer/Answer";
 import "./App.scss";
 
-type AppProps = {
-  domElement: Element;
-};
-
-interface AnswerProps {
-  answer: string;
-  vote: number;
-  id: number;
-}
 function App({ domElement }: AppProps) {
   const [question, setQuestion] = useState<string>("");
   const [answers, setAnswers] = useState<AnswerProps[]>([]);
   const [error, setError] = useState<Optional<string>>(null);
 
-  const incrementVoteCount = (id: number) => {
+  //increment vote count and add to local storage with exact id based on poll id and index
+  const incrementVoteCount = (id: string) => {
     let currentAnswers = answers.map((answer) => {
       if (answer.id === id) {
         answer.vote++;
       }
-
       window.localStorage.setItem(String(answer.id), String(answer.vote));
       return answer;
     });
@@ -33,29 +23,41 @@ function App({ domElement }: AppProps) {
   useEffect(() => {
     try {
       const questions = domElement.getAttribute("data-questions");
-
-      //add error handling if data attribute incorrect
-
-      const { question, answers } = JSON.parse(questions as string);
+      if (!questions) {
+        return;
+      }
+      const { question, answers, questionId } = JSON.parse(questions as string);
       setQuestion(question);
-      const answersArr = Object.values(answers).map((answer, i) => {
-        const initialCount = window.localStorage.getItem(String(i)) || 0;
-        return { answer: answer, vote: initialCount, id: i };
+      // create array of answers object with 3 props and set it to answers
+      const answersArr = answers.map((answer: AnswerProps, i: number) => {
+        //exactId makes sure different poll's questions on same page have unique key in localstorage
+        const exactId = String(i) + String(questionId);
+        const initialCount = window.localStorage.getItem(exactId) || 0;
+        return {
+          answer: answer,
+          vote: initialCount,
+          id: exactId,
+        };
       });
-
-      setAnswers(answersArr as []);
+      setAnswers(answersArr as AnswerProps[]);
     } catch (error) {
       setError(error as string);
+      console.log(error);
     }
   }, [domElement]);
 
   return (
     <div className="opinary-widget-app container w-75">
-      <div className="row">
-        {question && <h2 className="text-center">{question}</h2>}
+      <header>Poll Header</header>
+      <div className="opinary-widget-app__question-wrapper row">
+        {question && (
+          <h2 className="opinary-widget-app__question-title text-center">
+            {question}
+          </h2>
+        )}
         {answers &&
           answers.map(({ answer, vote, id }) => (
-            <div className="col-4">
+            <div key={id} className="col-4 opinary-widget-app__answer-wrapper">
               <Answer
                 answer={answer}
                 vote={vote}
@@ -65,8 +67,12 @@ function App({ domElement }: AppProps) {
             </div>
           ))}
       </div>
+      {error && (
+        <div className="opinary-widget-app__answer-wrapper">
+          The following error has occurred: {error}
+        </div>
+      )}
     </div>
   );
 }
-
 export default App;
