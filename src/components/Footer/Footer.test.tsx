39d5d0516bf7dfd0349a54881c6afc09efa39c70
Johnnybar <johnnybareket@gmail.com>
2022-02-25 12:50:04 +0000

    develop - fixes non-uniform tests for footer and header

--- src/components/Footer/Footer.test.tsx
+++ src/components/Footer/Footer.test.tsx
@@ -1,9 +1,24 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import { unmountComponentAtNode } from "react-dom";
+
 import Footer from "./Footer";
 
-test("renders learn react link", () => {
-  render(<Footer />);
-  const footerText = screen.getByText(/We help/i);
-  expect(footerText).toBeInTheDocument();
+describe("Footer", () => {
+  let container: any;
+  beforeEach(() => {
+    container = document.createElement("div");
+    document.body.appendChild(container);
+  });
+
+  afterEach(() => {
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+  });
+  test("Renders Footer", () => {
+    render(<Footer />);
+    const footerText = screen.getByText(/We help/i);
+    expect(footerText).toBeInTheDocument();
+  });
 });
--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,9 +1,23 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import { unmountComponentAtNode } from "react-dom";
 import Header from "./Header";
 
-test("renders learn react link", () => {
-  render(<Header />);
-  const headerText = screen.getByText(/Opinary Poll/i);
-  expect(headerText).toBeInTheDocument();
+describe("Header", () => {
+  let container: any;
+  beforeEach(() => {
+    container = document.createElement("div");
+    document.body.appendChild(container);
+  });
+
+  afterEach(() => {
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+  });
+  test("Renders Header", () => {
+    render(<Header />);
+    const headerText = screen.getByText(/Opinary Poll/i);
+    expect(headerText).toBeInTheDocument();
+  });
 });
